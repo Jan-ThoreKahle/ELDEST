6149cf10e8228e357355d461f04901be3d3bed04
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-05-30 11:56:29 +0200

    move complex integration to its own module

---
+++ complex_integration.py
@@ -0,0 +1,46 @@
+##########################################################################
+#                       COMPLEX INTEGRATION                              #
+##########################################################################
+# Purpose:                                                               #
+#          - A python module to calculate integrals of complex numbers.  #
+#                                                                        #
+##########################################################################
+# written by: Elke Fasshauer May 2018                                    #
+##########################################################################
+
+import scipy
+import scipy.integrate as integrate
+
+#-------------------------------------------------------------------------
+#   integration
+
+
+def complex_quadrature(func, a, b, **kwargs):
+    def real_func(x):
+        return scipy.real(func(x))
+    def imag_func(x):
+        return scipy.imag(func(x))
+    real_integral = integrate.quad(real_func, a, b, **kwargs)
+    imag_integral = integrate.quad(imag_func, a, b, **kwargs)
+    return (real_integral[0] + 1j*imag_integral[0], real_integral[1:],
+            imag_integral[1:])
+# This function was taken from:
+# https://stackoverflow.com/questions/5965583/use-scipy-integrate-quad-to-integrate-complex-numbers
+
+def complex_double_quadrature(outer, inner, a, b, gfun, hfun, **kwargs):
+    first_real = lambda y,x: scipy.real(outer(x)) * scipy.real(inner(y))
+    sec_real   = lambda y,x: - scipy.imag(outer(x)) * scipy.imag(inner(y))
+    first_imag = lambda y,x: scipy.imag(outer(x)) * scipy.real(inner(y))
+    sec_imag   = lambda y,x: scipy.real(outer(x)) * scipy.imag(inner(y))
+
+    def temp_ranges(*args):
+        return [gfun(args[0]) if callable(gfun) else gfun,
+                hfun(args[0]) if callable(hfun) else hfun]
+
+    first_real_integral = integrate.nquad(first_real, [temp_ranges, [a,b]])
+    sec_real_integral   = integrate.nquad(sec_real, [temp_ranges, [a,b]])
+    first_imag_integral = integrate.nquad(first_imag, [temp_ranges, [a,b]])
+    sec_imag_integral   = integrate.nquad(sec_imag, [temp_ranges, [a,b]])
+    return (first_real_integral[0] + sec_real_integral[0]
+            + 1j*first_imag_integral[0] + 1j*sec_imag_integral[0],
+            first_real_integral[1:])
--- eldest.py
+++ eldest.py
@@ -16,6 +16,7 @@ import scipy
 import scipy.integrate as integrate
 import numpy as np
 import sciconv
+import complex_integration as ci
 
 #-------------------------------------------------------------------------
 # Input parameters
@@ -54,38 +55,6 @@ Omega= 13.5
 #-------------------------------------------------------------------------
 # Definitions of reusable functions
 #-------------------------------------------------------------------------
-#   integration
-
-
-def complex_quadrature(func, a, b, **kwargs):
-    def real_func(x):
-        return scipy.real(func(x))
-    def imag_func(x):
-        return scipy.imag(func(x))
-    real_integral = integrate.quad(real_func, a, b, **kwargs)
-    imag_integral = integrate.quad(imag_func, a, b, **kwargs)
-    return (real_integral[0] + 1j*imag_integral[0], real_integral[1:],
-            imag_integral[1:])
-# This function was taken from:
-# https://stackoverflow.com/questions/5965583/use-scipy-integrate-quad-to-integrate-complex-numbers
-
-def complex_double_quadrature(outer, inner, a, b, gfun, hfun, **kwargs):
-    first_real = lambda y,x: scipy.real(outer(x)) * scipy.real(inner(y))
-    sec_real   = lambda y,x: - scipy.imag(outer(x)) * scipy.imag(inner(y))
-    first_imag = lambda y,x: scipy.imag(outer(x)) * scipy.real(inner(y))
-    sec_imag   = lambda y,x: scipy.real(outer(x)) * scipy.imag(inner(y))
-
-    def temp_ranges(*args):
-        return [gfun(args[0]) if callable(gfun) else gfun,
-                hfun(args[0]) if callable(hfun) else hfun]
-    
-    first_real_integral = integrate.nquad(first_real, [temp_ranges, [a,b]])
-    sec_real_integral   = integrate.nquad(sec_real, [temp_ranges, [a,b]])
-    first_imag_integral = integrate.nquad(first_imag, [temp_ranges, [a,b]])
-    sec_imag_integral   = integrate.nquad(sec_imag, [temp_ranges, [a,b]])
-    return (first_real_integral[0] + sec_real_integral[0]
-            + 1j*first_imag_integral[0] + 1j*sec_imag_integral[0],
-            first_real_integral[1:])
 
 #-------------------------------------------------------------------------
 # physical defintions of functions
@@ -107,7 +76,7 @@ f = lambda t2, t1: np.exp(t1 * complex(Gamma_eV/2,Er_eV)) * np.exp(t2 * complex(
 fun1 = lambda t1: np.exp(t1 * complex(Gamma_eV/2,Er_eV))
 fun2 = lambda t2: np.exp(t2 * complex(Gamma_eV/2, Er_eV + E_kin_eV))
 
-I = complex_double_quadrature(fun1,fun2, -TX_s/2, TX_s/2, lambda x: x, lambda x: TX_s/2)
+I = ci.complex_double_quadrature(fun1,fun2, -TX_s/2, TX_s/2, lambda x: x, lambda x: TX_s/2)
 
 print I
 
