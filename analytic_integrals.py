e6222031b150e349ab95e727a2831a5466e30a13
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-05-31 23:55:10 +0200

    define integral7_13 and integral_8 functions

--- analytic_integrals.py
+++ analytic_integrals.py
@@ -14,7 +14,8 @@ import numpy as np
 # Integral 3
 def integral_3(Vr, rdg, E_kin, TX, res, res_kin, t):
     I = - Vr * rdg / res_kin * (1./res * (np.exp((t-TX/2) * res) - 1)
-                              + complex(0,1./E_kin) * (np.exp(-(t-TX/2) * res) - 1))
+                              + complex(0,1./E_kin)
+                                * (np.exp(-1j*(t-TX/2)*E_kin) - 1))
     return I
 
 # Integral 4
@@ -28,4 +29,21 @@ def integral_6_12(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
     dm = delta - TL/2
     I = - Vr * rdg / res_kin * (1./res * (np.exp((dm-TX/2) * res) - 1)
                             + complex(0,1./E_kin) * (np.exp(-(dm-TX/2) * res) - 1))
+    return I
+
+# Integral 7 and 13
+def integral_7_13(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
+    dm = delta - TL/2
+    I = - Vr * rdg / (res * reskin) \
+          * (1 - np.exp((dm-TX/2) * reskin) - np.exp(-(dm-TX/2) * res)
+             + np.exp(1j*(dm-TX/2) * E_kin))
+    return I
+
+# Integral 8
+def integral_8(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
+    dm = delta - TL/2
+    I = - Vr * rdg / res_kin \
+        * (1./res * (np.exp((t-TX/2) * res) - np.((dm-TX/2) * res))
+           + complex(0,1./E_kin) * (np.exp(-(t-TX/2) * res)
+                                    - np.(-(dm-TX/2) * E_kin)))
     return I
