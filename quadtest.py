962f4914ade132dd7cb65e0b5ff4f41aded20d06
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-06-03 13:51:25 +0200

    add integrals to quadrature testing

--- quadtest.py
+++ quadtest.py
@@ -14,8 +14,36 @@ g = lambda y: np.exp(1j*y)
 
 I = ci.complex_double_quadrature(f,g,0,np.pi,lambda x: 0, lambda x: x)
 print I
+print "Result: ((107.29833110495294+481.8424899722883j), (1.1640029546930332e-11,))"
 
 I = ci.complex_double_quadrature(f,g,0,np.pi, 0, np.pi)
 print I
 
-print "Result: ((107.29833110495294+481.8424899722883j), (1.1640029546930332e-11,))"
+print '-------------------'
+f = lambda x: np.exp(2*x)
+g = lambda y: np.exp(complex(2,1) * y)
+I = ci.complex_double_quadrature(f,g,0,np.pi, 0, np.pi)
+print I
+#((-57350.06262733066+28675.031313665328j), (1.3068783974514146e-09,))
+print '-------------------'
+
+print '-------------------'
+f = lambda x: np.exp(complex(2,1) * x)
+g = lambda y: np.exp(-complex(1,1) * y)
+I = ci.complex_double_quadrature(f,g,0,np.pi, 0, np.pi)
+print I
+#((-57350.06262733066+28675.031313665328j), (1.3068783974514146e-09,))
+print '-------------------'
+
+#-------------------------------------------------------
+alpha = 2
+beta = 1
+gamma = 2
+delta = 2
+
+f = lambda x: np.exp(complex(alpha,beta)*x)
+g = lambda y: np.exp(complex(gamma,delta)*y)
+
+I = ci.complex_double_quadrature(f,g,0,np.pi,lambda x: x, lambda x: np.pi)
+print I
+
