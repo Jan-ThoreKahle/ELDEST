1cab5946e746938bc74cdde3e8469ff546e8dd2f
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-06-05 21:52:46 +0200

    add analytic integrals for the direct ionization

---
+++ dir_anal_integ.py
@@ -0,0 +1,45 @@
+##########################################################################
+#                       ANALYTIC INTEGRALS direct ionization             #
+##########################################################################
+# Purpose:                                                               #
+#          - A python module to calculate integrals of complex numbers.  #
+#                                                                        #
+##########################################################################
+# written by: Elke Fasshauer May 2018                                    #
+##########################################################################
+
+import numpy as np
+#-------------------------------------------------------------------------
+#   integration
+# Integral 3
+def integral_3(cdg, E_kin, TX, t):
+    I = 1j * cdg * complex(0,1./E_kin) * (1 - np.exp(1j*(t-TX/2)*E_kin))
+    return I
+
+# Integral 5
+def integral_5(cdg, E_kin, TX, TL, delta, t):
+    dm = delta - TL/2
+    I = 1j * cdg \
+        complex(0,1./E_kin) * (np.exp(-1j*(dm-TX/2)*E_kin) - 1)
+
+# Integral 6
+def integral_6(cdg, E_kin, TX, TL, delta, t):
+    dm = delta - TL/2
+    I = 1j * cdg \
+        complex(0,1./E_kin) * (np.exp(-1j*(t-dm)*E_kin) - 1)
+    return I
+
+# Integral 9
+def integral_9(cdg, E_kin, TX, TL, delta):
+    dm = delta - TL/2
+    I = 1j * cdg \
+        complex(0,1./E_kin) * (np.exp(-1j*TL*E_kin) - 1)
+    return I
+
+# Integral 10
+def integral_10(cdg, E_kin, TX, TL, delta, t):
+    dm = delta - TL/2
+    I = 1j * cdg \
+        complex(0,1./E_kin) * (np.exp(-1j*(t-dm)*E_kin) - np.exp(-1j*TL*E_kin))
+    return I
+
