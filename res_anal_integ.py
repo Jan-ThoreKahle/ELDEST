7e0873046888343ee7cd58a6a1882eceaa0744fc
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-06-05 21:51:01 +0200

    only evaluate integrals in the analytic integral routines
    prefactors are multiplied later

--- res_anal_integ.py
+++ res_anal_integ.py
@@ -14,28 +14,28 @@ import complex_integration as ci
 #   integration
 # Integral 3
 def integral_3(Vr, rdg, E_kin, TX, res, res_kin, t):
-    I = - Vr * rdg / res_kin * (1./res * (np.exp((t-TX/2) * res) - 1)
+    I = 1. / res_kin * (1./res * (np.exp((t-TX/2) * res) - 1)
                               - complex(0,1./E_kin)
                                 * (np.exp(-1j*(t-TX/2)*E_kin) - 1))
     return I
 
 # Integral 4
 def integral_4(Vr, rdg, E_kin, TX, res, res_kin, t):
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (1 - np.exp((t-TX/2) * res_kin) - np.exp(-(t-TX/2) * res)
              + np.exp(1j*(t-TX/2) * E_kin))
 
 # Integral 6 and 12 (they are the same)
 def integral_6_12(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
     dm = delta - TL/2
-    I = - Vr * rdg / res_kin * (1./res * (np.exp((dm-TX/2) * res) - 1)
+    I = 1.0 / res_kin * (1./res * (np.exp((dm-TX/2) * res) - 1)
                             - complex(0,1./E_kin) * (np.exp(-1j*(dm-TX/2) * E_kin) - 1))
     return I
 
 # Integral 7 and 13
 def integral_7_13(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
     dm = delta - TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (1 - np.exp((dm-TX/2) * res_kin) - np.exp(-(dm-TX/2) * res)
              + np.exp(1j*(dm-TX/2) * E_kin))
     return I
@@ -43,7 +43,7 @@ def integral_7_13(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
 # Integral 8
 def integral_8(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dm = delta - TL/2
-    I = - Vr * rdg / res_kin \
+    I = 1. / res_kin \
         * (1./res * (np.exp((t-TX/2) * res) - np.exp((dm-TX/2) * res))
            - complex(0,1./E_kin) * (np.exp(-1j*(t-TX/2) * E_kin)
                                     - np.exp(-1j*(dm-TX/2) * E_kin)))
@@ -52,7 +52,7 @@ def integral_8(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
 # Integral 9
 def integral_9(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dm = delta - TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1. / (res * res_kin) \
           * (np.exp((t-dm) * res)
              - np.exp((t-TX/2) * res) * np.exp(1j*(dm-TX/2) * E_kin)
              - 1
@@ -62,7 +62,7 @@ def integral_9(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
 # Integral 10 but without the extra phase facot coming from the second pulse
 def integral_10(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dm = delta - TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (np.exp(1j*(t-dm) * E_kin)
              - np.exp((t-dm) * res)
              - np.exp(-(t-dm) * res_kin)
@@ -73,7 +73,7 @@ def integral_10(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
 def integral_14(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
     dm = delta - TL/2
     dp = delta + TL/2
-    I = - Vr * rdg / res_kin \
+    I = 1.0 / res_kin \
         * (1./res * (np.exp((dp-TX/2) * res) - np.exp((dm-TX/2) * res))
            - complex(0,1./E_kin) * (np.exp(-1j*(dp-TX/2) * E_kin)
                                     - np.exp(-1j*(dm-TX/2) * E_kin)))
@@ -83,7 +83,7 @@ def integral_14(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
 def integral_15(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
     dm = delta - TL/2
     dp = delta + TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (np.exp(TL * res)
              - np.exp((dp-TX/2) * res) * np.exp(1j*(dm-TX/2) * E_kin)
              - 1
@@ -94,7 +94,7 @@ def integral_15(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
 def integral_16(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
     dm = delta - TL/2
     dp = delta + TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (np.exp(1j*TL * E_kin)
              - np.exp(TL * res)
              - np.exp(-TL * res_kin)
@@ -104,7 +104,7 @@ def integral_16(Vr, rdg, E_kin, TX, TL, delta, res, res_kin):
 # Integral 17
 def integral_17(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dp = delta + TL/2
-    I = - Vr * rdg / res_kin \
+    I = 1.0 / res_kin \
         * (1./res * (np.exp((t-TX/2) * res) - np.exp((dp-TX/2) * res))
            - complex(0,1./E_kin) * (np.exp(-1j*(t-TX/2) * E_kin)
                                     - np.exp(-1j*(dp-TX/2) * E_kin)))
@@ -114,7 +114,7 @@ def integral_17(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
 def integral_18(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dm = delta - TL/2
     dp = delta + TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (np.exp((t-dm) * res)
              - np.exp((t-TX/2) * res) * np.exp(1j*(dm-TX/2) * E_kin)
              - np.exp(TL * res)
@@ -125,7 +125,7 @@ def integral_18(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
 def integral_19(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dm = delta - TL/2
     dp = delta + TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (np.exp((t-dp) * res) * np.exp(-1j*TL * E_kin)
              - np.exp((t-dm) * res)
              - 1
@@ -136,7 +136,7 @@ def integral_19(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
 def integral_20(Vr, rdg, E_kin, TX, TL, delta, res, res_kin, t):
     dm = delta - TL/2
     dp = delta + TL/2
-    I = - Vr * rdg / (res * res_kin) \
+    I = 1.0 / (res * res_kin) \
           * (np.exp(-1j*TL * E_kin)
              - np.exp((t-dm) * res) * np.exp(-1j*(t-dp-TL) * E_kin)
              - np.exp(-(t-dp) * res) * np.exp(-1j*TL * E_kin)
