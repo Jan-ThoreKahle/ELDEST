4d6a29fb0ea945dfa968587534bd8bb81d806384
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-06-19 10:23:40 +0200

    during the pulse works! :-) add after the pulse
    Here it is anticipated that Fx(t')=0 for t > TX/2
    
    This would not be a good idea for the resonant case.

--- Omega_eldest.py
+++ Omega_eldest.py
@@ -29,16 +29,16 @@ cdg_au        = 0.5           # transition dipole moment into any continuum stat
 
 # parameters of the investigated system
 # the ground state energy is being defined as Eg = 0
-Er_eV         = 150.0          # resonance energy in eV
-E_fin_eV      = 135.0         # final state energy in eV
+Er_eV         = 150.0         # resonance energy in eV
+E_fin_eV      =  70.0         # final state energy in eV
 
 tau_s         =  400.0E-18       # lifetime
 
 # laser parameters
 Omega_eV      = 150.0          #
 TX_s          = 250.0E-18       # duration of the XUV pulse in seconds
-n_X           = 3
-I_X           = 5.0E20        # intensity of the XUV pulse in W/cm^2
+n_X           = 7
+I_X           = 5.0E12        # intensity of the XUV pulse in W/cm^2
 #A0X           = 1.0           # amplitude of the XUV pulse
 
 omega_eV      = 1.0           # IR pulse
@@ -53,8 +53,8 @@ phi           = 0
 q             = 5
 
 # parameters of the simulation
-tmax_s        = 2.0E-16       # simulate until time tmax in seconds
-timestep_s    = 50E-18        # evaluate expression every timestep_s seconds 
+tmax_s        = 3.0E-16       # simulate until time tmax in seconds
+timestep_s    = 20E-18        # evaluate expression every timestep_s seconds 
 E_min_eV      = 130
 E_max_eV      = 170
 E_step_eV     = 2.0           # energy difference between different evaluated Omegas
@@ -140,14 +140,14 @@ fp_TX = lambda tau: np.pi/(2j*TX_au) * ( - np.exp(2j*np.pi* (TX_au/2 - tau)/TX_a
 FX_TX = lambda tau: - A0X * np.cos(Omega_au * (TX_au/2 - tau)) * fp_TX(tau) + A0X * Omega_au * np.sin(Omega_au * (TX_au/2 - tau)) * f_TX(tau)
 
 # functions for the norm
-f_t1  = lambda t1: 1./4 * ( np.exp(2j * np.pi * (t1) / TX_au)
+f_t1  = lambda t1: 1./4 * ( np.exp(2j * np.pi * t1 / TX_au)
                       + 2
-                      + np.exp(-2j * np.pi * (t1) /TX_au) )
+                      + np.exp(-2j * np.pi * t1 /TX_au) )
 
-fp_t1 = lambda t1: np.pi/(2j*TX_au) * ( - np.exp(2j*np.pi* (t1)/TX_au)
-                                     + np.exp(-2j*np.pi* (t1) /TX_au) )
+fp_t1 = lambda t1: np.pi/(2j*TX_au) * ( - np.exp(2j*np.pi* t1/TX_au)
+                                     + np.exp(-2j*np.pi* t1 /TX_au) )
 
-FX_t1 = lambda t1: - A0X * np.cos(Omega_au * (t1)) * fp_t(t1) + A0X * Omega_au * np.sin(Omega_au * (t1)) * f_t(t1)
+FX_t1 = lambda t1: - A0X * np.cos(Omega_au * t1) * fp_t(t1) + A0X * Omega_au * np.sin(Omega_au * (t1)) * f_t(t1)
 
 # IR pulse
 A_IR = lambda t3: A0L * np.sin(np.pi * (t3 - delta_t_au + TL_au/2) / TL_au)**2 \
@@ -164,9 +164,11 @@ fun_t_2 = lambda tau: np.exp(complex(0,E_kin_au) * tau) * FX_t(tau)
 fun_TX2_1 = lambda tau: np.exp(-tau * res) * FX_TX(tau)
 fun_TX2_2 = lambda tau: np.exp(complex(0,E_kin_au) * tau) * FX_TX(tau)
 
-#norm
+#direct ionization
 fun_t_dir_1 = lambda t1: FX_t1(t1) * np.exp(1j * E_fin_au * t1) \
                                    * np.exp(1j * E_kin_au * (t1-t_au))
+fun_TX2_dir_1 = lambda t1: FX_t1(t1) * np.exp(1j * E_fin_au * t1) \
+                                   * np.exp(1j * E_kin_au * (t1-TX_au/2))
 
 
 #-------------------------------------------------------------------------
@@ -203,18 +205,13 @@ while ((t_au <= TX_au/2) and (t_au <= tmax_au)):
     while (E_kin_au <= Omega_au):
 
 # integral 1
-# other integration variable
         I = ci.complex_quadrature(fun_t_dir_1, (-TX_au/2), t_au)
 
         dir_J = prefac_dir * I[0]
 
         J = (0
-        #    + res_J
-        #    + indir_J
              + dir_J
              )
-
-        #print 'J = ', J
 
         square = np.absolute(J)**2
 
@@ -231,24 +228,42 @@ while ((t_au <= TX_au/2) and (t_au <= tmax_au)):
 
 
 
-##-------------------------------------------------------------------------
-#while (t_au >= TX_au/2 and t_au <= (delta_t_au - TL_au/2) and (t_au <= tmax_au)):
-##-------------------------------------------------------------------------
-#    outfile.write('between the pulses \n')
-#
-#    outlines = []
-#
-#        square = np.absolute(L)**2
-#
-#        string = in_out.prep_output(square, Omega_au, t_au)
-#        outlines.append(string)
-#        
-#        Omega_au = Omega_au + Omega_step_au
-#    
-#    
-#    in_out.doout_1f(pure_out,outlines)
-#
-#    t_au = t_au + timestep_au
+#-------------------------------------------------------------------------
+while (t_au >= TX_au/2 and t_au <= (delta_t_au - TL_au/2) and (t_au <= tmax_au)):
+#-------------------------------------------------------------------------
+    outfile.write('between the pulses \n')
+
+    outlines = []
+    E_kin_au = 0
+    
+    print 't_au = ', t_au
+    while (E_kin_au <= Omega_au):
+
+# integral 1
+        I1 = ci.complex_quadrature(fun_TX2_dir_1, (-TX_au/2), TX_au/2)
+        #dir_integral_3 = aidir.integral_3(E_fin=E_fin_au, E_kin=E_kin_au,
+        #                                  TX=TX_au, t=t_au)
+
+        dir_J = prefac_dir * (I1[0]
+                #              + dir_integral_3 # this could be 0
+                              )
+
+        J = (0
+             + dir_J
+             )
+
+        square = np.absolute(J)**2
+
+        string = in_out.prep_output(square, E_kin_au, t_au)
+        outlines.append(string)
+        
+        E_kin_au = E_kin_au + E_step_au
+
+    
+    
+    in_out.doout_1f(pure_out,outlines)
+
+    t_au = t_au + timestep_au
 
 
 outfile.close
