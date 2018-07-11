49ef2b5e3125504d7f3f8487730ba4dc5427cc3c
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-07-11 14:41:33 +0200

    change interface for input reading in photoelectron script

--- photoelectron.py
+++ photoelectron.py
@@ -36,7 +36,7 @@ pure_out = open('full.dat', mode='w')
 (rdg_au, cdg_au, 
  Er_eV, E_fin_eV, tau_s,
  Omega_eV, n_X, I_X, X_sinsq, X_gauss,
- omega_eV, n_L, I_L, delta_t_s, phi, q,
+ omega_eV, n_L, I_L, delta_t_s, shift_step_s, phi, q,
  tmax_s, timestep_s, E_step_eV,
  E_min_eV, E_max_eV) = in_out.read_input(infile, outfile)
 
