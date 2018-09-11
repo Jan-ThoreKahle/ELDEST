d80b53eb3cd3415388d68e8f4ff82b9ab1d89a0d
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-09-11 13:56:31 +0200

    correct interface of input reading in the two other scripts

--- photoelectron.py
+++ photoelectron.py
@@ -38,7 +38,7 @@ outfile.write("The results were obtained with photoelectron.py \n")
  omega_eV, n_L, I_L, delta_t_s, shift_step_s, phi, q,
  tmax_s, timestep_s, E_step_eV,
  E_min_eV, E_max_eV,
- integ) = in_out.read_input(infile, outfile)
+ integ, integ_outer) = in_out.read_input(infile, outfile)
 
 
 #-------------------------------------------------------------------------
