cd121f3bf67fbfd6fcdc405ac67e3b9b6aada66b
Elke Fasshauer <elke.fasshauer@gmail.com>
2018-09-12 12:55:54 +0200

    remove redundant play part

--- Omega_eldest.py
+++ Omega_eldest.py
@@ -203,25 +203,6 @@ IR_after = lambda t1:  np.exp(-1j * p_au**2/2 * (t_au - t1)) \
 
 #IR_after = lambda t1:  np.exp(-1j * p_au**2/2 * (t_au - t1)) \
 #                       * np.exp( -1j * p_au * A0L / 4
-#                       * (np.sin(np.pi - omega_au * (delta_t_au + TL_au/2)
-#                                 - phi)
-#                           / (2*np.pi/TL_au - omega_au)
-#                          + np.sin(-2*np.pi/TL_au * (t1 - delta_t_au) + omega_au * t1
-#                                 + phi) 
-#                           / (2*np.pi/TL_au - omega_au)
-#                          + np.sin(np.pi + omega_au * (delta_t_au + TL_au/2)
-#                                 + phi) 
-#                           / (2*np.pi/TL_au + omega_au)
-#                          + np.sin(-2*np.pi/TL_au * (t1 - delta_t_au) - omega_au * t1
-#                                 - phi) 
-#                           / (2*np.pi/TL_au + omega_au)
-#                          + 2./omega_au * np.sin(omega_au * (delta_t_au + TL_au/2) + phi)
-#                          - 2./omega_au * np.sin(omega_au * t1 + phi)
-#                         )
-#                      )
-
-#IR_after = lambda t1:  np.exp(-1j * p_au**2/2 * (t_au - t1)) \
-#                       * np.exp( -1j * p_au * A0L / 4
 #                       * (
 #                          + 4./omega_au * np.sin(omega_au * (delta_t_au + TL_au/2) + phi)
 #                          - 4./omega_au * np.sin(omega_au * t1 + phi)
