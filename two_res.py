add a lot of debug information plus correct a sign error

two_res.py
@@ -276,15 +276,34 @@ print "VEr_a_au =", VEr_a_au
 print "VEr_b_au =", VEr_b_au
 
 r1 = np.sqrt(((E_plus - E_minus)**2 - (np.pi*V_plus**2 + np.pi*V_minus**2)**2)**2
-             + 4*np.pi**2 * (E_plus - E_minus)**2 * (V_plus**2 + V_minus**2)**2)
+             + 4*np.pi**2 * (E_plus - E_minus)**2 * (V_plus**2 - V_minus**2)**2)
 phi1 = np.arctan(2*np.pi * (E_plus - E_minus) * (V_plus**2 - V_minus**2)
                 / ((E_plus - E_minus)**2 - np.pi**2 * (V_plus**2 + V_minus**2)))
+
+print 'sqrt(r1) * cos(phi1/2) / 2', sciconv.hartree_to_ev(np.sqrt(r1) * np.cos(phi1/2) / 2)
+print 'sqrt(r1) * cos(phi1/2)', np.sqrt(r1) * np.cos(phi1/2)
+print 'sqrt(r1) * sin(phi1/2)', np.sqrt(r1) * np.sin(phi1/2)
+print 'np.pi/2 * (V_plus**2 + V_minus**2) = ', sciconv.hartree_to_ev(np.pi/2 * (V_plus**2 + V_minus**2))
+print "(e+ - e-)^2 = ", (E_plus - E_minus)**2
+print "pi^2 (V+^2 + V-^2)^2", np.pi**2 * (V_plus**2 + V_minus**2)**2
+print "Grenzwert = ", (E_plus - E_minus)**2 - np.pi**2 * (V_plus**2 + V_minus**2)**2
+print "Wurzel 1 = ", np.sqrt((E_plus - E_minus)**2 - np.pi**2 * (V_plus**2 + V_minus**2)**2)
+print "Wurzel 2 = ", np.sqrt((E_plus - E_minus)**2)
 
 # transition dipole moments
 plusdg  = A_a_plus * rdg_au + A_b_plus * rdg_au
 minusdg = A_a_minus * rdg_au + A_b_minus * rdg_au
 print "plusdg = ", plusdg
 print "minusdg = ", minusdg
+
+root1 = np.sqrt( (E_plus - E_minus + 1j*np.pi*V_plus**2)**2
+                -2*np.pi*(1j*(E_plus-E_minus) + np.pi*V_plus**2) * V_minus**2
+                -np.pi**2 * V_minus**4
+               )
+root2 = np.sqrt( (E_minus - E_plus + 1j*np.pi*V_plus**2)**2
+                -2*np.pi*(1j*(E_minus-E_plus) + np.pi*V_plus**2) * V_minus**2
+                -np.pi**2 * V_minus**4
+               )
 
 # auxiliary energies
 E1 = (E_plus + E_minus)/2 + 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
@@ -295,6 +314,19 @@ E3 = (E_plus + E_minus)/2 - 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
      + np.sqrt(r1)/2 * np.cos(phi1/2) - 1j*np.pi/2 * np.sin(phi1/2)
 E4 = (E_plus + E_minus)/2 - 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
      - np.sqrt(r1)/2 * np.cos(phi1/2) + 1j*np.pi/2 * np.sin(phi1/2)
+print "E1 = ", sciconv.hartree_to_ev(np.real(E1)), sciconv.hartree_to_ev(np.imag(E1))
+print "E2 = ", sciconv.hartree_to_ev(np.real(E2)), sciconv.hartree_to_ev(np.imag(E2))
+print "E3 = ", sciconv.hartree_to_ev(np.real(E3)), sciconv.hartree_to_ev(np.imag(E3))
+print "E4 = ", sciconv.hartree_to_ev(np.real(E4)), sciconv.hartree_to_ev(np.imag(E4))
+
+E1 = (E_plus + E_minus)/2 + 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
+     - root1 / 2
+E2 = (E_plus + E_minus)/2 + 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
+     + root1 / 2
+E3 = (E_plus + E_minus)/2 - 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
+     - root1 / 2
+E4 = (E_plus + E_minus)/2 - 1j * np.pi/2 * (V_plus**2 + V_minus**2) \
+     + root1 / 2
 print "E1 = ", sciconv.hartree_to_ev(np.real(E1)), sciconv.hartree_to_ev(np.imag(E1))
 print "E2 = ", sciconv.hartree_to_ev(np.real(E2)), sciconv.hartree_to_ev(np.imag(E2))
 print "E3 = ", sciconv.hartree_to_ev(np.real(E3)), sciconv.hartree_to_ev(np.imag(E3))