8d26bcad39244be1798758ba22e7f825b83b0752
Elke Fasshauer <elke.fasshauer@gmail.com>
2020-11-27 20:47:09 +0100

    remove old commented wellefkt functions

--- det_pot.py
+++ det_pot.py
@@ -30,22 +30,5 @@ while another == 'y':
 else:
     sys.exit()
 
-#def red_mass_au(mass1,mass2):
-#    red_mass_gmol = mass1 * mass2 / (mass1 + mass2)
-#    red_mass_au = sc.gmol_to_me(red_mass_gmol)
-#    return red_mass_au
-#
-#def eigenvalue(n, De, alpha, red_mass):
-#    En =   (n + 0.5) * alpha * np.sqrt(2*De / red_mass) \
-#         - (n + 0.5)**2 * alpha**2 / (2* red_mass)
-#    return En
-#
-#def sqrt_fact(real):
-#    if (np.absolute(1 - real) < 1.0E-7):
-#        return np.sqrt(real)
-#    elif real < 1.0:
-#        return np.sqrt(scipy.misc.factorial(real) )
-#    else:
-#        return np.sqrt(real) * sqrt_fact(real-1)
 
 
