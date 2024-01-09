869089328aaada51b2c7cf46fd126fb150e70954
Cakeotic <34921553+Cakeotic@users.noreply.github.com>
2024-01-09 16:55:23 +0100

    Update nuclear_dyn.py
    Added new input parameters to account for 1/R^ 6

--- nuclear_dyn.py
+++ nuclear_dyn.py
@@ -69,6 +69,7 @@ Xshape = 'convoluted'
 (rdg_au, cdg_au,
  Er_a_eV, Er_b_eV, tau_a_s, tau_b_s, E_fin_eV, tau_s, E_fin_eV_2, tau_s_2,
  interact_eV,
+ R_a0, a_R, b_R,
  Omega_eV, n_X, I_X, X_sinsq, X_gauss, Xshape,
  omega_eV, n_L, I_L, Lshape, delta_t_s, shift_step_s, phi, q, FWHM_L,
  tmax_s, timestep_s, E_step_eV,
